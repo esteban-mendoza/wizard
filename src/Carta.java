1d4f5a9e1306dbbb7441747412403a1d26302e42
Carlos de la Rosa <charly55dlr@ciencias.unam.mx>
2022-04-13 16:52:46 -0500

    Documentacion de carta y nueva clase Palo.

--- src/Carta.java
+++ src/Carta.java
@@ -1,17 +1,34 @@
+/**
+ * CLase que representa una carta de una baraja.
+ */
 public class Carta {
-
+    /**
+     * Atributos
+     *
+     */
     private int numero;
     private String palo;
 
+    /**
+     * Constructor publico.
+     * @param numero
+     * @param palo
+     */
     public Carta(int numero, String palo){
         this.numero = numero;
         this.palo = palo;
     }
-
+    /**
+     * Metodo que devuelve el numero de la carta.
+     * @return numero
+     */
     public int getNumero(){
         return numero;
     }
-
+    /**
+     * Metodo que devuelve el palo de la carta.
+     * @return palo
+     */
     public String getPalo(){
         return palo;
     }
@@ -30,48 +47,80 @@ public class Carta {
         String color = "";
         switch(palo){
             case "Humano":
-                color = "azul";
+                color = "\u001B[34mazul";
                 break;
             case "Elfo":
-                color = "verde";
+                color = "\u001B[32mverde";
                 break;
             case "Enano":
-                color = "rojo";
+                color = "\u001B[31rojo";
                 break;
             case "Gigante":
-                color = "amarillo";
+                color = "\u001B[33mamarillo";
                 break;
-            case "Magos":
-            case "Bufones":
-                color = "blanco";
+            case "Mago":
+            case "Bufon":
+                color = "\u001B[0mblanco";
                 break;
         }
         return numero + " de " + color;
     }
 
+    /**
+     * Compara si dos cartas son iguales.
+     * @param carta
+     * @return
+     */
     public boolean equals(Carta carta){
         return numero == carta.numero && palo.equals(carta.palo);
     }
-
+    /**
+     * Compara si una carta es menor que otra.
+     * @param carta
+     * @return
+     */
     public boolean menorQue(Carta carta){
         return numero < carta.numero;
     }
-
+    /**
+     * Compara si una carta es mayor que otra.
+     * @param carta
+     * @return
+     */
     public boolean mayorQue(Carta carta){
         return numero > carta.numero;
     }
-
+    /**
+     * Compara si una carta es menor o igual que otra.
+     * @param carta
+     * @return
+     */
     public boolean menorOIgualQue(Carta carta){
         return numero <= carta.numero;
     }
-
+    /**
+     * Compara si una carta es mayor o igual que otra.
+     * @param carta
+     * @return
+     */
     public boolean mayorOIgualQue(Carta carta){
         return numero >= carta.numero;
     }
-
+    /**
+     * Compara si una carta es igual a otra.
+     * @param carta
+     * @return
+     */
     public boolean esIgual(Carta carta){
         return numero == carta.numero;
     }
 
-
+    /**
+     * Compara si una carta es distinta a otra.
+     * @param carta
+     * @return
+     */
+    public boolean esDistinto(Carta carta){
+        return numero != carta.numero;
+    }
 }
---
+++ src/Palo.java
@@ -0,0 +1,107 @@
+import java.util.Objects;
+
+/**
+* CLase que representa a un palo.
+*/
+
+public class Palo {
+    /**
+    * Atributos de la clase.
+    */
+    private String nombre;
+    private String color;
+    private Boolean lider;
+    private Boolean triunfo;
+
+    /**
+    * Constructor de la clase.
+    */
+    public Palo(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+        this.lider = false;
+        this.triunfo = false;
+    }
+
+    /**
+     * Metodo que devuelve el nombre del palo.
+     * @return
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Metodo que devuelve el color del palo.
+     * @return
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     *
+     * Metodo que devuelve si el palo es lider.
+     * @return
+     */
+    public Boolean getLider() {
+        return lider;
+    }
+
+    /**
+     * Metodo que establece si el palo es lider.
+     * @param lider
+     */
+    public void setLider(Boolean lider) {
+        this.lider = lider;
+    }
+    /**
+     * Metodo que devuelve si el palo es de triunfo.
+     * @return
+     */
+    public Boolean getTriunfo() {
+        return triunfo;
+    }
+    /**
+     * Metodo que establece si el palo es de triunfo.
+     * @param triunfo
+     */
+    public void setTriunfo(Boolean triunfo) {
+        this.triunfo = triunfo;
+    }
+
+    /**
+     * Metodo que devuelve el palo en formato String.
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Palo{" +
+                "nombre='" + nombre + '\'' +
+                ", color='" + color + '\'' +
+                ", lider=" + lider +
+                ", triunfo=" + triunfo +
+                '}';
+    }
+
+    public boolean equals (Palo palo) {
+        if (this.nombre.equals(palo.getNombre()) && this.color.equals(palo.getColor())) {
+            return true;
+        }
+        return false;
+    }
+    public boolean esLider(){
+        if (this.lider == true && this.triunfo == false) {
+            return true;
+        }
+        return false;
+    }
+    public boolean esTriunfo(){
+        if (this.triunfo == true && this.lider == false) {
+            return true;
+        }
+        return false;
+    }
+}
+
