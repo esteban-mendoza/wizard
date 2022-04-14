7f0008f0208a6fd204093464220e74af5df18fc0
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-14 10:11:26 -0500

    Clase Baraja con métodos para revolver, devolver la carta de la cima, y getters.

--- src/Baraja.java
+++ src/Baraja.java
@@ -3,14 +3,55 @@ import utils.colecciones.Lista;
 /**
  * Pila de cartas.
  */
-public class Baraja extends Lista<Carta> {
+public class Baraja {
 
+    private Lista<Carta> baraja;
 
     /**
      * Constructor que crea una baraja de 60 cartas.
      */
     public Baraja() {
+        baraja = new Lista<>();
+
         String[] palos = {"humanos", "elfos", "enanos", "gigantes"};
         int[] numeros = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        for (String palo: palos) {
+            for (int numero: numeros) {
+                baraja.add(new Carta(numero, palo));
+            }
+        }
     }
+
+    /**
+     * Devuelve la carta en la cima de la baraja.
+     * @return La carta de la cima de la baraja.
+     */
+    public Carta sacarCarta() {
+        return baraja.popFirst();
+    }
+
+    /**
+     * Revuelve la baraja.
+     */
+    public void revolver() {
+        baraja.shuffle();
+    }
+
+    /**
+     * Devuelve la baraja.
+     * @return la baraja.
+     */
+    public Lista<Carta> getCartas() {
+        return baraja;
+    }
+
+    /**
+     * Devuelve la baraja.
+     * @return la baraja.
+     */
+    public Lista<Carta> getBaraja() {
+        return baraja;
+    }
+
 }
