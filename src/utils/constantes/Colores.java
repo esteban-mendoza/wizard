cbddc9087df05ddd2f90a2d1d36fd1a2790f37b1
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-14 09:33:24 -0500

    Agregué constantes de colores.

---
+++ src/utils/constantes/Colores.java
@@ -0,0 +1,14 @@
+package utils.constantes;
+
+/**
+ * Clase que contiene los colores de los componentes de la interfaz
+ */
+public class Colores {
+
+    public static final String NEUTRO = "\u001B[0m";
+    public static final String ROJO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARILLO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";
+    public static final String BLANCO = "\u001B[37m";
+}
