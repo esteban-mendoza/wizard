f5082fa12da7b7235376b8b89e5acc9383b8e863
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-14 08:50:51 -0500

    Refactoricé Palo

--- src/Palo.java
+++ src/Palo.java
@@ -8,110 +8,32 @@ public class Palo {
     /**
     * Atributos de la clase.
     */
-    private String nombre;
+    private final String nombre;
     private String color;
-    private Boolean lider;
-    private Boolean triunfo;
 
     /**
     * Constructor de la clase.
     */
-    public Palo(String nombre, String color) {
+    public Palo(String nombre) {
         this.nombre = nombre;
-        this.color = color;
-        this.lider = false;
-        this.triunfo = false;
-    }
 
-    /**
-     * Metodo que devuelve el nombre del palo.
-     * @return
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * Metodo que devuelve el color del palo.
-     * @return
-     */
-    public String getColor() {
-        return color;
-    }
-
-    /**
-     *
-     * Metodo que devuelve si el palo es lider.
-     * @return
-     */
-    public Boolean getLider() {
-        return lider;
-    }
-
-    /**
-     * Metodo que establece si el palo es lider.
-     * @param lider
-     */
-    public void setLider(Boolean lider) {
-        this.lider = lider;
-    }
-    /**
-     * Metodo que devuelve si el palo es de triunfo.
-     * @return
-     */
-    public Boolean getTriunfo() {
-        return triunfo;
-    }
-    /**
-     * Metodo que establece si el palo es de triunfo.
-     * @param triunfo
-     */
-    public void setTriunfo(Boolean triunfo) {
-        this.triunfo = triunfo;
-    }
-
-    /**
-     * Metodo que devuelve el palo en formato String.
-     *
-     * @return
-     */
-    @Override
-    public String toString() {
-        return "Palo{" +
-                "nombre='" + nombre + '\'' +
-                ", color='" + color + '\'' +
-                ", lider=" + lider +
-                ", triunfo=" + triunfo +
-                '}';
-    }
-
-    public boolean equals (Palo palo) {
-        if (this.nombre.equals(palo.getNombre()) && this.color.equals(palo.getColor())) {
-            return true;
+        switch (nombre) {
+            case "enanos": this.color = "\u001B[31m"; break;
+            case "elfos": this.color = "\u001B[32m"; break;
+            case "gigantes": this.color = "\u001B[33m"; break;
+            case "humanos": this.color = "\u001B[34m"; break;
         }
-        return false;
     }
 
     /**
-     * metodo que verifica si el palo es lider.
-     * @return
+     * Determina si dos palos son iguales.
+     * @param otroPalo Palo con el que se compara.
+     * @return true si son iguales, false en caso contrario.
      */
-    public boolean esLider(){
-        if (this.lider == true && this.triunfo == false) {
-            return true;
-        }
-        return false;
+    public boolean equals(Palo otroPalo) {
+        return this.nombre.equals(otroPalo.nombre);
     }
 
-    /**
-     * metodo que verifica si el palo es de triunfo.
-     * @return
-     */
-    public boolean esTriunfo(){
-        if (this.triunfo == true && this.lider == false) {
-            return true;
-        }
-        return false;
-    }
+
 }
 
