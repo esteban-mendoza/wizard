ae50fe8b94df834599aa31bcc2ff1e3d42703bb8
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-14 08:52:41 -0500

    Agregué métodos para inicializar Partida

--- src/Partida.java
+++ src/Partida.java
@@ -1,66 +1,60 @@
+import utils.colecciones.ListaCircular;
+
 public class Partida {
 
     private int numeroJugadores;
     private int numeroRondas;
-    private Baraja baraja;
-    private Jugador[] jugadores;
+    private ListaCircular<Jugador> jugadores;
     private Historial historial;
     private CLI cli;
 
+    /**
+     * Constructor de la clase Partida
+     */
     public Partida() {
+        pedirNumeroJugadores();
+        determinarNumeroRondas();
+        instanciarJugadores();
+        historial = new Historial();
+    }
 
+    /**
+     * Pedir número de jugadores
+     */
+    private void pedirNumeroJugadores() {
         cli = CLI.getInstance();
-
+        numeroJugadores = cli.pedirEntero("Ingrese el numero de jugadores: ", 3, 6);
     }
 
-    public void inicializarJugadores() {
-        return;
+    /**
+     * Determinar número de rondas usando el número de jugadores
+     */
+    private void determinarNumeroRondas() {
+        switch (numeroJugadores) {
+            case 3:
+                numeroRondas = 20;
+                break;
+            case 4:
+                numeroRondas = 15;
+                break;
+            case 5:
+                numeroRondas = 12;
+                break;
+            case 6:
+                numeroRondas = 10;
+                break;
+        }
     }
 
-    public void iniciarPartida() {
-        return;
-    }
-    public void mostrarPuntajes() {
-        return;
-    }
-
-    public int getNumeroJugadores() {
-        return numeroJugadores;
-    }
-
-    public void setNumeroJugadores(int numeroJugadores) {
-        this.numeroJugadores = numeroJugadores;
+    /**
+     * Método que instancia los jugadores
+     */
+    private void instanciarJugadores() {
+        jugadores = new ListaCircular<>();
+        for (int i = 0; i < numeroJugadores; i++) {
+            jugadores.add(new Jugador());
+        }
     }
 
-    public int getNumeroRondas() {
-        return numeroRondas;
-    }
 
-    public void setNumeroRondas(int numeroRondas) {
-        this.numeroRondas = numeroRondas;
-    }
-
-    public Baraja getBaraja() {
-        return baraja;
-    }
-
-    public void setBaraja(Baraja baraja) {
-        this.baraja = baraja;
-    }
-
-    public Jugador[] getJugadores() {
-        return jugadores;
-    }
-
-    public void setJugadores(Jugador[] jugadores) {
-        this.jugadores = jugadores;
-    }
-
-    public Historial getHistorial() {
-        return historial;
-    }
-
-    public void setHistorial(Historial historial) {
-        this.historial = historial;
-    }
 }
