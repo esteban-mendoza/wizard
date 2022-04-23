de1dd8fbc43b098e1681bf59344f83241e2acd94
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-22 20:13:56 -0500

    Modifiqué métodos de interacción del cli para agregar una opción que permita al usuario acceder a opciones extra (historial, resumen, ayuda) en cualquier momento o no.

--- src/CLI.java
+++ src/CLI.java
@@ -1,3 +1,5 @@
+import utils.colecciones.Lista;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -51,11 +53,13 @@ public class CLI {
     /**
      * Pide un entero al usuario usando el mensaje provisto, y si el entero está entre min y max y lo devuelve.
      *
-     * <p>Si la cadena introducida por el usuario es:
+     * <p>
+     * Si <code>extras == true</code> se pueden introducir las siguientes cadenas:
      * <ul>
      *     <li>r -> se muestra un resumen del juego y vuelve a pedir la cadena.</li>
      *     <li>q -> se muestra un resumen del juego y se termina el mismo.</li>
      *     <li>h -> se muestra el historial del juego y vuelve a pedir la cadena.</li>
+     *     <li>? -> se muestra la ayuda del juego y vuelve a pedir la cadena.</li>
      * </ul></p>
      *
      * @param mensaje El mensaje que se usa para pedir el entero.
@@ -63,7 +67,7 @@ public class CLI {
      * @param max El máximo valor que puede tomar el entero.
      * @return El entero introducido por el usuario.
      */
-    public int pedirEntero(String mensaje, int min, int max) {
+    public int pedirEntero(String mensaje, int min, int max, boolean extras) {
         int numero;
 
         while (true) {
@@ -71,19 +75,26 @@ public class CLI {
             try {
                 String linea = input.readLine();
 
-                if (linea.equals("h")) {
-                    Juego juego = getJuego();
-                    juego.mostrarHistorial();
-                }
+                if (extras) {
+                    if (linea.equals("h")) {
+                        Juego juego = getJuego();
+                        juego.mostrarHistorial();
+                    }
 
-                if (linea.equals("r")) {
-                    Juego juego = getJuego();
-                    juego.mostrarResumen();
-                }
+                    if (linea.equals("r")) {
+                        Juego juego = getJuego();
+                        juego.mostrarResumen();
+                    }
 
-                if (linea.equals("q")) {
-                    Juego juego = getJuego();
-                    juego.terminar();
+                    if (linea.equals("q")) {
+                        Juego juego = getJuego();
+                        juego.terminar();
+                    }
+
+                    if (linea.equals("?")) {
+                        Juego juego = getJuego();
+                        juego.mostrarAyuda();
+                    }
                 }
 
                 numero = Integer.parseInt(linea);
@@ -105,18 +116,19 @@ public class CLI {
      * Pide un entero al usuario usando el mensaje provisto, y si el entero está en el array de válidos lo devuelve.
      *
      * <p>
-     * Si la cadena introducida por el usuario es:
+     * Si <code>extras == true</code> se pueden introducir las siguientes cadenas:
      * <ul>
      *     <li>r -> se muestra un resumen del juego y vuelve a pedir la cadena.</li>
      *     <li>q -> se muestra un resumen del juego y se termina el mismo.</li>
      *     <li>h -> se muestra el historial del juego y vuelve a pedir la cadena.</li>
+     *     <li>? -> se muestra la ayuda del juego y vuelve a pedir la cadena.</li>
      * </ul></p>
      *
      * @param mensaje El mensaje que se usa para pedir el entero.
      * @param validos Los valores válidos que puede tomar el entero.
      * @return El entero introducido por el usuario.
      */
-    public int pedirEntero(String mensaje, int[] validos) {
+    public int pedirEntero(String mensaje, Lista<Integer> validos, boolean extras) {
         int numero;
 
         while (true) {
@@ -124,38 +136,34 @@ public class CLI {
             try {
                 String linea = input.readLine();
 
-                if (linea.equals("h")) {
-                    Juego juego = getJuego();
-                    juego.mostrarHistorial();
-                }
+                if (extras) {
+                    if (linea.equals("h")) {
+                        Juego juego = getJuego();
+                        juego.mostrarHistorial();
+                    }
 
-                if (linea.equals("r")) {
-                    Juego juego = getJuego();
-                    juego.mostrarResumen();
-                }
+                    if (linea.equals("r")) {
+                        Juego juego = getJuego();
+                        juego.mostrarResumen();
+                    }
 
-                if (linea.equals("q")) {
-                    Juego juego = getJuego();
-                    juego.terminar();
+                    if (linea.equals("q")) {
+                        Juego juego = getJuego();
+                        juego.terminar();
+                    }
+
+                    if (linea.equals("?")) {
+                        Juego juego = getJuego();
+                        juego.mostrarAyuda();
+                    }
                 }
 
                 numero = Integer.parseInt(linea);
-                for (int valido: validos) {
-                    if (numero == valido)
-                        return numero;
-                }
-
-                StringBuilder validosString = new StringBuilder("{");
-                for (int i = 0; i < validos.length; i++) {
-                    validosString.append(validos[i]);
-
-                    if (i < validos.length - 1)
-                        validosString.append(", ");
-                }
-                validosString.append("}");
-                System.out.println("El número debe ser alguno de los siguientes: " + validosString + ".");
+                if (validos.contains(numero))
+                    return numero;
 
 
+                System.out.println("El número debe ser alguno de los siguientes: " + validos + ".");
 
             } catch (IOException e) {
                 System.out.println("Error al leer la entrada. Intente de nuevo.");
@@ -169,16 +177,21 @@ public class CLI {
      * Pide una cadena al usuario usando el mensaje provisto, y si la cadena no está vacía y es una cadena
      * válida, la devuelve. De otro modo, lanza una excepción.
      *
-     * Si la cadena introducida por el usuario es:
-     * - r -> se muestra un resumen del juego y vuelve a pedir la cadena.
-     * - q -> se muestra un resumen del juego y se termina el mismo.
-     * - h -> se muestra el historial del juego y vuelve a pedir la cadena.
+     * <p>
+     * Si <code>extras == true</code> se pueden introducir las siguientes cadenas:
+     *
+     * <ul>
+     * <li>r -> se muestra un resumen del juego y vuelve a pedir la cadena.</li>
+     * <li>q -> se muestra un resumen del juego y se termina el mismo.</li>
+     * <li>h -> se muestra el historial del juego y vuelve a pedir la cadena.</li>
+     * <li>? -> se muestra la ayuda del juego y vuelve a pedir la cadena.</li>
+     * </ul></p>
      *
      * @param mensaje El mensaje que se usa para pedir la cadena.
      * @param validos Las formas válidas que puede tomar la cadena.
      * @return La cadena introducida por el usuario.
      */
-    public String pedirCadena(String mensaje, String[] validos) {
+    public String pedirCadena(String mensaje, Lista<String> validos, boolean extras) {
         String cadena;
 
         while (true) {
@@ -187,17 +200,36 @@ public class CLI {
             try {
                 cadena = input.readLine();
 
+                if (extras) {
+                    if (cadena.equals("h")) {
+                        Juego juego = getJuego();
+                        juego.mostrarHistorial();
+                    }
+
+                    if (cadena.equals("r")) {
+                        Juego juego = getJuego();
+                        juego.mostrarResumen();
+                    }
+
+                    if (cadena.equals("q")) {
+                        Juego juego = getJuego();
+                        juego.terminar();
+                    }
+
+                    if (cadena.equals("?")) {
+                        Juego juego = getJuego();
+                        juego.mostrarAyuda();
+                    }
+                }
+
                 if (validos == null) {
                     return cadena;
                 }
 
-                for (String valido : validos) {
-                    if (cadena.equals(valido)) {
-                        return cadena;
-                    }
-                }
+                if (validos.contains(cadena))
+                    return cadena;
 
-                System.out.println("La opción introducida no es válida. Intente de nuevo.");
+                System.out.println("La cadena debe ser alguna de las siguientes: " + validos + ".");
 
             } catch (IOException e) {
                 System.out.println("Error al leer la entrada. Intente de nuevo.");
