646335c0b8a761a47ff1a3815b0de8f0408de461
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-26 01:22:12 -0500

    Clase main que instancia el juego y lo juega.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import utils.colecciones.Lista;
-import utils.colecciones.ListaCircular;
-
 /**
  * Punto de entrada de la aplicación.
  */
@@ -8,26 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Juego juego = new Juego();
-//        juego.inicializarPartida();
-//        juego.jugar();
-//        juego.terminar();
+        Juego juego = new Juego();
+        juego.jugar();
 
-
-        ListaCircular<Integer> lista = new ListaCircular<>();
-        System.out.println(lista);
-
-        lista.add(1);
-        lista.add(2);
-        lista.add(3);
-        lista.add(4);
-
-        System.out.println(lista);
-
-        Lista<Integer> lista2 = lista.clone();
-        System.out.println("lista.clone(): " +  lista2);
-
-        lista.reverse();
-        System.out.println(lista);
     }
 }
