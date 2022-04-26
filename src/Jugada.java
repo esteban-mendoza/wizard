4fae5a2f46b837c21d1d17cd96d7e014dfa679b0
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-26 01:20:35 -0500

    Clase Jugada con métodos para imprimirla.

--- src/Jugada.java
+++ src/Jugada.java
@@ -7,11 +7,26 @@ public class Jugada {
         this.jugador = jugador;
     }
 
+    /**
+     * Método que devuelve la carta de la jugada.
+     * @return Carta de la jugada.
+     */
     public Carta getCarta(){
         return carta;
     }
 
+    /**
+     * Método que devuelve el jugador de la jugada.
+     * @return Jugador de la jugada.
+     */
     public Jugador getJugador(){
         return jugador;
+    }
+
+    /**
+     * Método que imprime la jugada.
+     */
+    public void mostrarJugada() {
+        System.out.println(jugador.getNombre() + " jugó " + carta + ".");
     }
 }
