b4c39c20c9e315475dcfc9c4951f4dae8970df55
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-26 01:23:32 -0500

    Clase Resultado para guardar información de cada Ronda para cada jugador.

---
+++ src/Resultado.java
@@ -0,0 +1,67 @@
+public class Resultado {
+    private int apuesta;
+    private int nTrucosGanados;
+    private int puntos;
+    private int resultado;
+
+    public Resultado(int puntos, int apuesta) {
+        this.apuesta = apuesta;
+        this.nTrucosGanados = 0;
+        this.puntos = puntos;
+        this.resultado = 0;
+    }
+
+    /**
+     * Devuelve la apuesta del jugador
+     * @return la apuesta
+     */
+    public int getApuesta() {
+        return apuesta;
+    }
+
+    /**
+     * Aumenta en 1 el número de trucos ganados
+     */
+    public void ganarTruco() {
+        nTrucosGanados++;
+    }
+
+    /**
+     * Devuelve el resultado de la ronda para el jugador
+     * @return el resultado de la ronda para el jugador
+     */
+    private int resultado() {
+        if (apuesta == nTrucosGanados)
+            return 20 + 10 * nTrucosGanados;
+        else
+            return -10 * Math.abs(apuesta - nTrucosGanados);
+    }
+
+    /**
+     * Actualiza el número de puntos que le corresponden al jugador al final de la ronda
+     */
+    public void actualizarPuntos() {
+        resultado = resultado();
+        puntos += resultado;
+    }
+
+    /**
+     * Devuelve el número de puntos que le corresponden al jugador al final de la ronda
+     * @return el número de puntos que le corresponden al jugador al final de la ronda
+     */
+    public int getPuntos() {
+        return puntos;
+    }
+
+
+    @Override
+    public String toString() {
+        String stringResultado;
+        if (resultado > 0)
+            stringResultado = "+" + resultado;
+        else
+            stringResultado = "" + resultado;
+        return "Resultado: " + stringResultado + ". Puntuación actual: " + puntos + ".";
+    }
+
+}
