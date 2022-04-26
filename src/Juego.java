59bbf29c5eaa7132150b2a6f059d4f4535eaf337
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-26 01:20:10 -0500

    Métodos para terminar, resumir, mostrar ayuda y mostrar historial.

--- src/Juego.java
+++ src/Juego.java
@@ -9,13 +9,9 @@ public class Juego {
     private Partida partida;
 
     public Juego() {
+        System.out.println("Bienvenidx a Wizard (ᚹᛇᛉᛖᚱᛞ).");
         cli = CLI.getInstance();
-    }
-
-    /**
-     * Crea una nueva partida.
-     */
-    public void inicializarPartida() {
+        cli.setJuego(this);
         partida = new Partida();
     }
 
@@ -23,30 +19,49 @@ public class Juego {
      * Jugar la partida.
      */
     public void jugar() {
+        mostrarAyuda();
+        partida.iniciarPartida();
+        terminar();
     }
 
     /**
      * Muestra el historial de la partida.
      */
     public void mostrarHistorial() {
-
-       // Debe iterar por las rondas, e imprimir cada truco.
-        return;
+        System.out.println("==========================================================");
+        System.out.println("Historial de la partida:");
+        partida.mostrarHistorial();
+        System.out.println("==========================================================");
     }
 
     /**
      * Muestra resumen de la partida.
      */
     public void mostrarResumen() {
-        return;
+        partida.mostrarResumen();
     }
 
     /**
      * Terminar juego.
      */
     public void terminar() {
+        System.out.println("==========================================================");
+        System.out.println("Ha concluido la partida.");
         mostrarResumen();
-        return;
+    }
+
+    /**
+     * Muestra las opciones que el usuario puede elegir durante el juego.
+     */
+    public void mostrarAyuda() {
+        System.out.println("==========================================================");
+        System.out.println("A partir de este momento, puede ingresar cualquiera de las siguientes opciones\n" +
+                "cuando se le pida alguna acción:");
+        System.out.println("\th - Mostrar historial de la partida.");
+        System.out.println("\tr - Mostrar resumen de la partida.");
+        System.out.println("\tq - Terminar juego.");
+        System.out.println("\t? - Mostrar este mensaje de ayuda.");
+        System.out.println("Para ver un video explicando las reglas de Wizard diríjase a: https://youtu.be/mYO4lealSes");
     }
 
 }
