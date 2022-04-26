7a35dd235022b3860732242cc4b257a3906cfc89
Jorge Esteban M <jorge.mendoza@enroutesystems.com>
2022-04-26 01:19:20 -0500

    Métodos para agregar rondas al historial y mostrar el historial.

--- src/Historial.java
+++ src/Historial.java
@@ -1,3 +1,4 @@
+import utils.colecciones.IteradorLista;
 import utils.colecciones.Lista;
 
 public class Historial {
@@ -6,6 +7,26 @@ public class Historial {
 
     public Historial() {
         historial = new Lista<Ronda>();
+    }
+
+    /**
+     * Agrega una ronda al historial
+     * @param ronda Ronda a agregar
+     */
+    public void addRonda(Ronda ronda){
+        historial.add(ronda);
+    }
+
+    /**
+     * Muestra el historial de rondas
+     */
+    public void mostrarHistorial() {
+
+        IteradorLista<Ronda> it = historial.iterador();
+        while (it.hasNext()) {
+            Ronda ronda = it.next();
+            ronda.mostrarRonda();
+        }
     }
 
 
